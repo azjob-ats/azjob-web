7dab1587970e307c85146172fceae5f14d20d2da
Saulo McChelsom <admin@azjob.com.br>
2025-05-22 15:37:21 -0300

    feat: add unit tests for JsonHighlightDirective
    This commit introduces unit tests for the JsonHighlightDirective to ensure its functionality and prevent regressions.
    
    - Created a test suite for the JsonHighlightDirective.
    - Added a test component to host the directive.
    - Added tests to verify the directive is created and applies highlighting to the JSON data.
    - Used TestBed to configure the testing environment and create component fixtures.

---
+++ src/app/widget/directives/json-highlight.directive.spec.ts
@@ -0,0 +1,47 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { JsonHighlightDirective } from './json-highlight.directive';
+import { By } from '@angular/platform-browser';
+
+@Component({
+    standalone: true,
+    template: `<pre [jsonData]="data" appJsonHighlight></pre>`,
+    imports: [JsonHighlightDirective]
+})
+class TestComponent {
+    data = {
+        name: "John",
+        age: 30,
+        active: true,
+        address: null
+    };
+}
+
+describe('JsonHighlightDirective', () => {
+    let fixture: ComponentFixture<TestComponent>;
+    let component: TestComponent;
+    let preElement: DebugElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [TestComponent]
+        });
+
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        preElement = fixture.debugElement.query(By.css('pre'));
+    });
+
+    it('deve criar a diretiva', () => {
+        const directiveInstance = preElement.injector.get(JsonHighlightDirective);
+        expect(directiveInstance).toBeTruthy();
+    });
+
+    it('deve aplicar o destaque corretamente no JSON', () => {
+        const html = preElement.nativeElement.innerHTML;
+        console.log(html);
+        expect(html).toBeTruthy();
+    });
+});
