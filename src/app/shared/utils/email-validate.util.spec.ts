ef0d5f46a90db38589ae4b10dcc65896777abeeb
Saulo McChelsom <admin@azjob.com.br>
2025-05-22 17:04:42 -0300

    Adds email validation util unit tests
    Adds unit tests for the email validation utility to ensure that it correctly validates email addresses.
    
    Tests include cases for valid and invalid email formats, improving the reliability and maintainability of the validation logic.

---
+++ src/app/shared/utils/email-validate.util.spec.ts
@@ -0,0 +1,34 @@
+import { EmailValidateUtil } from './email-validate.util';
+import { of, throwError } from 'rxjs';
+
+describe('EmailValidateUtil', () => {
+  it('deve retornar true para um e-mail válido', done => {
+    const email = 'teste@exemplo.com';
+
+    EmailValidateUtil.isValidEmail(email).subscribe({
+      next: (result) => {
+        expect(result).toBe(true);
+        done();
+      },
+      error: () => {
+        fail('Não deveria disparar erro para e-mail válido');
+        done();
+      }
+    });
+  });
+
+  it('deve disparar erro para um e-mail inválido', done => {
+    const email = 'invalido-email';
+
+    EmailValidateUtil.isValidEmail(email).subscribe({
+      next: () => {
+        fail('Deveria disparar erro para e-mail inválido');
+        done();
+      },
+      error: (err) => {
+        expect(err).toBe('Este e-mail não é valido');
+        done();
+      }
+    });
+  });
+});
