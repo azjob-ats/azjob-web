9d33f3f356406b46bd63c2cfe61ac9ad33f7c6f4
Saulo McChelsom <admin@azjob.com.br>
2025-05-22 15:07:47 -0300

    feat: add unit tests for LoadingSpinnerDirective
    This commit introduces unit tests for the LoadingSpinnerDirective to ensure its functionality and prevent regressions.
    
    - Created a test suite for the LoadingSpinnerDirective.
    - Added tests to verify the directive's behavior when the `loading` input changes.
    - Tested the addition and removal of the spinner element based on the `loading` state.
    - Tested scenarios where the buttonElement is null.
    - Used TestBed to configure the testing environment and create component fixtures.

---
+++ src/app/widget/directives/loading-spinner.directive.spec.ts
@@ -0,0 +1,119 @@
+import { Component, DebugElement } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { LoadingSpinnerDirective } from './loading-spinner.directive'; // ajuste o caminho conforme necessário
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    standalone: true,
+    template: `
+    <p-button
+        label="Novo usuario"
+        severity="info"
+        styleClass="w-full h-full"
+        [appLoadingSpinner]="isLoading"
+      />
+    `,
+    imports: [
+        LoadingSpinnerDirective,
+        ButtonModule,
+        CommonModule
+    ]
+})
+class TestComponent {
+    loading = false;
+}
+
+describe('LoadingSpinnerDirective', () => {
+    let fixture: ComponentFixture<TestComponent>;
+    let component: TestComponent;
+    let directiveInstance: LoadingSpinnerDirective;
+    let buttonDebugEl: DebugElement;
+    let nativeButton: HTMLButtonElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                TestComponent,
+                LoadingSpinnerDirective
+            ]
+        });
+
+        fixture = TestBed.createComponent(TestComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+
+        buttonDebugEl = fixture.debugElement.query(By.css('button'));
+        nativeButton = buttonDebugEl.nativeElement;
+        directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+    });
+
+    it('should create the directive instance', () => {
+        expect(directiveInstance).toBeTruthy();
+    });
+
+    it('deve adicionar o spinner quando loading for true via [ngOnChanges]', () => {
+        const directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+        directiveInstance.loading = true;
+        directiveInstance.ngOnChanges();
+        const spinner = fixture.nativeElement.querySelector('.spinner-icon');
+        expect(nativeButton.hasAttribute('disabled')).toBe(true);
+        expect(spinner).toBeTruthy();
+    });
+
+    it('deve remover o spinner quando loading for false após ter sido true', () => {
+        const directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+        directiveInstance.loading = true;
+        directiveInstance.ngOnChanges();
+
+
+        directiveInstance['removeSpinner']();
+
+        const spinner = nativeButton.querySelector('.spinner-icon');
+        expect(nativeButton.hasAttribute('disabled')).toBe(false);
+        expect(spinner).toBeNull();
+    });
+
+    it('Não deve remover o spinner quando buttonElement null', () => {
+        const directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+        directiveInstance.loading = true;
+        directiveInstance.ngOnChanges();
+
+        directiveInstance['buttonElement'] = null;
+        directiveInstance['removeSpinner']();
+
+        const spinner = fixture.nativeElement.querySelector('.spinner-icon');
+        expect(nativeButton.hasAttribute('disabled')).toBe(true);
+        expect(spinner).toBeTruthy();
+    });
+
+    it('Não deve adicionar o spinner quando buttonElement null', () => {
+        const directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+        directiveInstance.loading = true;
+
+        directiveInstance['buttonElement'] = null;
+        directiveInstance['addSpinner']();
+
+        const spinner = nativeButton.querySelector('.spinner-icon');
+        expect(spinner).toBeNull();
+    });
+
+    it('deve remover o spinner quando loading for false', () => {
+        const directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+        directiveInstance.loading = false;
+
+        directiveInstance.ngOnChanges();
+        const spinner = nativeButton.querySelector('.spinner-icon');
+        expect(spinner).toBeNull();
+    });
+
+    it('deve adicionar o spinner quando loading for true via [ngAfterViewInit]', () => {
+        const directiveInstance = buttonDebugEl.injector.get(LoadingSpinnerDirective);
+        directiveInstance.loading = true;
+        directiveInstance.ngAfterViewInit();
+        const spinner = fixture.nativeElement.querySelector('.spinner-icon');
+        expect(nativeButton.hasAttribute('disabled')).toBe(true);
+        expect(spinner).toBeTruthy();
+    });
+});
