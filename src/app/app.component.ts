71c9ae8711c5f539bc5906a6b4329e15f1e66eda
Saulo McChelsom <admin@azjob.com.br>
2025-05-27 22:55:37 -0300

    Adds side navigation menu to the app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationTabTitleService } from '@domain/change-language/services/navigation-tab-title.service';
+import { SideNavigationMenuComponent } from '@widget/components/SideNavigationMenu/SideNavigationMenu.component';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, SideNavigationMenuComponent],
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `
+    <div class="grid nested-grid">
+        <div class="col-2 col-fixed m-0 p-0 ">
+            <div class="p-0 h-full">
+               <app-side-navigation-menu/>
+            </div>
+        </div>
+        <div class="col-10 m-0 p-0">
+            <div class="p-0">
+              <router-outlet></router-outlet>
+            </div>
+        </div>
+    </div>
+  `,
 })
 export class AppComponent {
   private titleService: NavigationTabTitleService = inject(NavigationTabTitleService);
