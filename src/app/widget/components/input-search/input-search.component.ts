090522f0731bd2e9b355eb7222fc9884070fb5c9
Saulo McChelsom <admin@azjob.com.br>
2025-06-04 23:45:14 -0300

    feat: simplify input-search component

--- src/app/widget/components/input-search/input-search.component.ts
+++ src/app/widget/components/input-search/input-search.component.ts
@@ -1,121 +1,19 @@
-import { Component, Input } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
 import { ImportsModule } from '@core/imports';
 
 @Component({
   selector: 'app-input-search',
   template: `
-      <p-iconfield class="w-full">
-        <p-inputicon  styleClass="inputIconTop">
-          <i class="pi pi-search black  font-bold" style="font-size: 20px; color: var(--p-background-color-inverse)"></i>
-        </p-inputicon>
-        <input type="text" pInputText id="firstname" placeholder="Procurar" pSize="large" class="w-full" #firstname />
-        <p-inputicon *ngIf="firstname.value" styleClass="inputIconTop">
-          <p-button (click)="firstname.value = ''" [rounded]="true" severity="primary" styleClass="p-0"
-            [disabled]="isDisabled">
-            <i class="pi pi-times" style="font-size: 10px; padding: 5px;"></i>
-          </p-button>
-        </p-inputicon>
-      </p-iconfield>
-   
-    <small class="err-small" *ngIf="formControl.hasError('required') && formControl.touched">
-      {{ erroRequired }}
-    </small>
-    <small class="err-small" *ngIf="formControl.hasError('minlength') && formControl.touched">
-      {{ erroFill }}
-    </small>
+  <input type="text" pInputText id="firstname" placeholder="Procurar" pSize="large" class="w-full" />
   `,
   styles: [
     `
       input {
         height: 40px;
       }
-
-
     `,
   ],
   standalone: true,
   imports: [ImportsModule],
 })
-export class InputSearchComponent {
-  @Input() public formControl!: FormControl<any>;
-  @Input() public title!: string;
-  @Input() public erroFill!: string;
-  @Input() public erroRequired!: string;
-  @Input() public isRequired = false;
-  @Input() public isDisabled = false;
-  @Input() public minLength!: number;
-  @Input() public maxLength!: number;
-  public isReady = false;
-
-  public ngOnChanges() {
-    setTimeout(() => {
-      this.isReady = true;
-    });
-  }
-
-  private initialControlValue: any;
-  private initialDisabledState!: boolean;
-  private initialValidator: any;
-
-  public ngOnInit(): void {
-    this.initialControlValue = this.formControl.value;
-    this.initialDisabledState = this.isDisabled;
-    this.initialValidator = this.isRequired ? Validators.required : null;
-
-    this.applyDisabledState(this.isDisabled);
-    this.applyMinLength(this.minLength);
-    this.applyMaxLength(this.maxLength);
-    this.applyRequiredValidator(this.isRequired);
-  }
-
-  private applyMinLength(min: number) {
-    if (min == null || min <= 0) {
-      return;
-    }
-
-    const existingValidators = this.formControl.validator ? [this.formControl.validator] : [];
-    this.formControl.setValidators([...existingValidators, Validators.minLength(min)]);
-    this.formControl.updateValueAndValidity();
-  }
-
-  private applyMaxLength(max: number) {
-    if (max == null || max <= 0) {
-      return;
-    }
-
-    const existingValidators = this.formControl.validator ? [this.formControl.validator] : [];
-    this.formControl.setValidators([...existingValidators, Validators.maxLength(max)]);
-    this.formControl.updateValueAndValidity();
-  }
-
-  public resetToInitialState(): void {
-    this.formControl.patchValue(this.initialControlValue);
-    this.applyRequiredValidator(this.initialValidator !== null);
-    this.applyDisabledState(this.initialDisabledState);
-    this.formControl.markAsUntouched();
-    this.formControl.updateValueAndValidity();
-  }
-
-  public enable(): void {
-    this.formControl.enable();
-  }
-
-  public disable(): void {
-    this.formControl.disable();
-  }
-
-  private applyDisabledState(shouldDisable: boolean): void {
-    shouldDisable ? this.disable() : this.enable();
-  }
-
-  private applyRequiredValidator(isRequired: boolean): void {
-    const existingValidators = this.formControl.validator ? [this.formControl.validator] : [];
-    if (isRequired) {
-      this.formControl.setValidators([...existingValidators, Validators.required]);
-    } else {
-      this.formControl.setValidators(existingValidators.filter(v => v !== Validators.required));
-    }
-    this.formControl.updateValueAndValidity();
-  }
-}
+export class InputSearchComponent { }
