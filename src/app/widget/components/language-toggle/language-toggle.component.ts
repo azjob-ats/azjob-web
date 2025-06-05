958d6b5e1d02b4407a7189b8aad3c6b0373cc8d1
Saulo McChelsom <admin@azjob.com.br>
2025-06-04 23:43:22 -0300

    feat: add language toggle component

---
+++ src/app/widget/components/language-toggle/language-toggle.component.ts
@@ -0,0 +1,53 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { LanguageService } from '@domain/change-language/services/language.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageTranslatorService } from '@domain/change-language/services/language-translator.service';
+import { Selection } from '@domain/change-language/interfaces/language.interface';
+import { LanguageApiMockservice } from '@domain/change-language/mocks/language.api.mock';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-language-toggle',
+  imports: [CommonModule, TranslateModule, ReactiveFormsModule, RouterModule],
+  providers: [LanguageApiMockservice, LanguageService, LanguageTranslatorService, TranslateService],
+  templateUrl: './language-toggle.component.html',
+  styleUrl: './language-toggle.component.scss',
+})
+export class LanguageToggleComponent implements OnInit {
+
+  public formControl = new FormControl('');
+  public selectEmpy = '';
+  public default = '';
+  public selections: Selection[] = [];
+  private translationService: LanguageTranslatorService = inject(LanguageTranslatorService);
+  private translate: TranslateService = inject(TranslateService);
+  private lang: LanguageService = inject(LanguageService);
+
+  public ngOnInit(): void {
+    this.selectEmpy = this.translate.instant('AZJOB.TEST.OPTION_EMPY');
+    this.default = this.translationService.getDefaultLang();
+
+    this.formControl.setValue(this.default);
+    this.lang.getAllLanguage().subscribe(res => {
+      res.forEach(value => {
+        this.selections.push({
+          description: value.language,
+          cod: value.prefix,
+        });
+      });
+    });
+  }
+
+  public selected($event: string) {
+    this.default = $event;
+    this.translationService.changeLang($event);
+  }
+
+  public onSelectionChange(event: Event) {
+    const value = (event.target as HTMLSelectElement).value;
+    this.selected(value);
+  }
+
+}
