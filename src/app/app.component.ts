bd673ce43c8c7baa9c68aeb5c1d2c4049a7d5ce4
Saulo McChelsom <admin@azjob.com.br>
2025-06-12 22:04:45 -0300

    fix: change by container menu side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavigationTabTitleService } from '@domain/change-language/services/navigation-tab-title.service';
-import { SideNavigationMenuComponent } from '@widget/components/SideNavigationMenu/SideNavigationMenu.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { SideNavigationMenuMainContainerComponent } from '@widget/components/SideNavigationMenu/container/side-navigation-menu-main-container/side-navigation-menu-main-container.component';
 
 @Component({
-  imports: [RouterModule, SideNavigationMenuComponent],
+  imports: [RouterModule, TranslateModule, SideNavigationMenuMainContainerComponent, TranslateModule],
   selector: 'app-root',
   template: `
     <div class="grid nested-grid">
         <div class="col-2 col-fixed m-0 p-0 ">
             <div class="p-0 h-full">
-               <app-side-navigation-menu/>
+               <app-side-navigation-menu-main-container/>
             </div>
         </div>
         <div class="col-10 m-0 p-0">
