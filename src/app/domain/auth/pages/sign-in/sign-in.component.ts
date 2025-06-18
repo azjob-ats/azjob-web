e13c62dc4262e00701dafddcfabd3959d3ca4733
Saulo McChelsom <admin@azjob.com.br>
2025-06-17 23:38:28 -0300

    feat: layout login

--- src/app/domain/auth/pages/sign-in/sign-in.component.ts
+++ src/app/domain/auth/pages/sign-in/sign-in.component.ts
@@ -1,9 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { InputPrimaryComponent } from
+  '@widget/components/input-primary/input-primary.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  imports: [
+    InputPrimaryComponent,
+    ButtonModule
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss',
 })
-export class SignInComponent {}
+export class SignInComponent {
+  public emailFormControl = new FormControl('');
+  public senhaFormControl = new FormControl('');
+  public emailFormOption = {
+    title: 'E-mail',
+    erroFill: 'O e-mail deve ter no mínimo 5 caracteres.',
+    erroRequired: 'e-mail é obrigatório.',
+    minLength: 5,
+    isRequired: true,
+    isDisabled: false,
+    maxLength: 100
+  }
+  public senhaFormOption = {
+    title: 'Senha',
+    erroFill: 'A senha deve ter no mínimo 5 caracteres.',
+    erroRequired: 'senha é obrigatório.',
+    minLength: 5,
+    isRequired: true,
+    isDisabled: false,
+    maxLength: 100
+  }
+}
