f29a5305b29348fc682f3e574bd9715ff0cc6a34
Saulo McChelsom <admin@azjob.com.br>
2025-06-22 23:22:13 -0300

    feat: layout

--- src/app/domain/auth/components/footer/footer.component.ts
+++ src/app/domain/auth/components/footer/footer.component.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, type OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LanguageSelectComponent } from '@widget/components/language-select/language-select.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule, LanguageSelectComponent],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   @Input() link!: string;
   @Input() text!: string;
+  @Input() showTextPrivacyAndPolicy: boolean = true;
   @Input() redirectText!: string;
   protected year: number = new Date().getFullYear();
 }
